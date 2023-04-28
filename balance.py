e5b688bbe0984c8302f7bede758c9dba52a6d3c3
zhwindy <kmust091314@gmail.com>
2023-04-28 17:22:50 +0800

    del address

--- balance.py
+++ balance.py
@@ -4,13 +4,10 @@ from web3 import Web3
 # NODE_URL = 'https://mainnet.infura.io/v3/2849c01a970f4ed082cb57be7f9db8bb'
 NODE_URL = 'https://goerli.infura.io/v3/2849c01a970f4ed082cb57be7f9db8bb'
 
-ADDRESS_FROM = "0x8e703608D7ab5144F8fE26D96193c10a9Aa2C287"
-ADDRESS_TO = "0xB46AAe4592b1C29DFB71D128169aC4980dD6E325"
+ADDRESS = "0xB46AAe4592b1C29DFB71D128169aC4980dD6E325"
 
 w3 = Web3(Web3.HTTPProvider(NODE_URL))
 
-balance_from = w3.fromWei(w3.eth.get_balance(ADDRESS_FROM), "ether")
-balance_to = w3.fromWei(w3.eth.get_balance(ADDRESS_TO), "ether")
+balance = w3.fromWei(w3.eth.get_balance(ADDRESS), "ether")
 
-print(f"The balance of { ADDRESS_FROM } is: { balance_from } ETH")
-print(f"The balance of { ADDRESS_TO } is: { balance_to } ETH")
+print(f"The balance of {ADDRESS} is: {balance} ETH")
