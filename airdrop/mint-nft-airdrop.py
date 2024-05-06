14d02397528aa9246229db175d570c229f4a77d8
zhwindy <kmust091314@gmail.com>
2024-05-06 16:49:21 +0800

    update

--- airdrop/mint-nft-airdrop.py
+++ airdrop/mint-nft-airdrop.py
@@ -250,6 +250,13 @@ def get_airdrop_address(airdrop_type=None):
     return records
 
 
+def get_airdrop_address_simple():
+    result = [
+        "0x7f1aa838fCCa95Ff940f2445886745E78252Fd45",
+    ]
+    return result
+
+
 def airdrop_nft(privKey, token_contract):
     """
     nft airdrop
@@ -286,9 +293,38 @@ def airdrop_nft(privKey, token_contract):
     conn.close()
 
 
+def airdrop_nft_simple(privKey, token_contract):
+    """
+    简单空投NFT,无需更新数据库
+    """
+    account = web3.Account.from_key(privKey)
+
+    airdrop_address = get_airdrop_address_simple()
+
+    nonce = int(rpc.get_transaction_count_by_address(account.address)['result'], 16)
+    for _ in range(100):
+        for address in airdrop_address:
+            if not address:
+                continue
+            if len(address) != 42:
+                continue
+            rt = mint_nft(privKey, token_contract, address, nonce=nonce)
+            if not rt:
+                time.sleep(2)
+                break
+            else:
+                if rt.get("result"):
+                    nonce += 1
+                else:
+                    break
+            time.sleep(2)
+
+
 if __name__ == '__main__':
-    nft_contract = '0x7517F010236bDbAf5E6B5049102566424B128B02'
-    nft_privKey = os.environ.get("PRIVATE_KEY_OL")
+    # nft_contract = '0x7517F010236bDbAf5E6B5049102566424B128B02'
+    nft_contract = '0x49353f03B9b097103EF35a6e1dCabEdC57623C7d'
+    nft_privKey = os.environ.get("PRIVATE_KEY_AD")
     # to_address = "0xCa261418513ea74Ef1416D5BBb1EDBe3d24dcB57"
     # print(mint_nft(nft_privKey, nft_contract, to_address=to_address))
-    airdrop_nft(nft_privKey, nft_contract)
+    # airdrop_nft(nft_privKey, nft_contract)
+    airdrop_nft_simple(nft_privKey, nft_contract)
